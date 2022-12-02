52e7c4a86006cc0530f84a346af6ad7093add7f9
Jose Martinez <33965736+xRayderx@users.noreply.github.com>
2022-12-02 17:38:20 -0400

    Add files via upload

---
+++ js/componentes/AppProfile.js
@@ -0,0 +1,25 @@
+app.component('app-profile',{
+
+    props:['result', 'isFavorite'],
+    methods: {
+        addFavorite(){
+            this.$emit('add-favorite')
+        },
+        removeFavorite(){
+            this.$emit('remove-favorite')
+        }
+    },
+    template: 
+    `
+    <div class="result">
+        <a v-if="isFavorite" href="#" class="result__toggle-favorite" @click="removeFavorite">Eliminar favoritos &#9746;</a>
+        <a v-else href="#" class="result__toggle-favorite" @click="addFavorite">Agregar a favoritos &#9733;</a>
+        <h2 class="result__name">{{ result.name }}</h2>
+        <img v-bind:src="result.avatar_url" :alt="result.name" class="result__avatar">
+        <p class="result__bio">{{ result.bio }}
+            <br>
+            <a v-bind:href="result.blog" target="_blank" class="result__blog">{{ result.blog }}</a> 
+        </p>
+    </div>
+    `
+})
---
+++ js/main.js
@@ -0,0 +1,88 @@
+const API = "https://api.github.com/users/";
+
+const requestMaxTimeMs = 3000;
+
+/* Instancia Vue */
+const app = Vue.createApp({
+    data(){
+        return{
+            search: null,
+            result: null,
+            error: null,
+            favorites: new Map(),
+        };
+    },
+    created(){
+        const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"));
+        if(savedFavorites?.length){
+            const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]));
+            this.favorites = favorites;
+        }
+    },
+    computed: {
+        isFavorite(){
+            return this.favorites.has(this.result.login);
+        },
+        allFavorites(){
+            return Array.from(this.favorites.values());
+        },
+    },
+    methods: {
+        async doSearch() {
+            const foundInFavorites = this.favorites.get(this.search);
+
+            const shouldRequestAgain = (() => {
+
+                if(!!foundInFavorites){
+                    const { lastRequest } = foundInFavorites;
+                    return (new Date().getTime() - new Date(lastRequest).getTime() > requestMaxTimeMs);
+                }
+                return false
+            })(); //IIFE
+
+            if(!!foundInFavorites && !shouldRequestAgain){
+                console.log("Encontrado y estamos utilizando la version cacheada.");
+                return (this.result = foundInFavorites);
+            }
+
+            await this.doRequest()
+            if(foundInFavorites) foundInFavorites.lastRequest = new Date();
+
+        },
+
+        async doRequest(){
+            try{
+                console.log('No se encuentra o la version cacheada es muy vieja.');
+                this.result = this.error = null;
+                const response = await fetch(API + this.search);
+                if(!response.ok) throw new Error('User not found');
+                const data = await response.json();
+                this.result = data;
+            } catch(error){
+                this.error = error;
+            } finally{
+                this.search = null;
+            }
+        },
+
+        addFavorite(){
+            this.result.lastRequest = new Date();
+            this.favorites.set(this.result.login, this.result);
+            this.updateStorage();
+        },
+        removeFavorite(){
+            this.favorites.delete(this.result.login);
+            this.updateStorage();
+        },
+        showFavorite(favorite){
+            this.result = favorite;
+        },
+        checkFavorite(id){
+            return this.result?.login === id
+        },
+        updateStorage(){
+            window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites));
+        },
+    },
+});
+
